5d1ddd8370aebf2a664a486d5f05adca1f4b5dd7
houxh <houxuehua49@gmail.com>
2016-05-10 20:10:39 +0800

    服务器地址配置

--- kefu/Config.h
+++ kefu/Config.h
@@ -9,16 +9,24 @@
 #ifndef Config_h
 #define Config_h
 
+
+#ifndef DEBUG
+
 //外网
 #define APPID 1670
 #define IM_API @"http://api.gobelieve.io"
 #define IM_HOST @"imnode.gobelieve.io"
 #define KEFU_API @"http://api.kefu.gobelieve.io"
 
+#else
+
 //内网
-//#define APPID 1453
-//#define IM_API @"http://192.168.1.101";
-//#define IM_HOST @"192.168.1.101";
-//#define KEFU_API @"http://192.168.33.10:60001"
+#define APPID 1453
+#define IM_API @"http://192.168.1.103"
+#define IM_HOST @"192.168.1.103"
+#define KEFU_API @"http://192.168.1.103:60001"
+
+#endif
+
 
 #endif /* Config_h */
