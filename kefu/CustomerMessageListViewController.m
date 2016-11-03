fbfd8bc5d814ded058a68fc52b6e3dac177e45e8
houxh <houxuehua49@gmail.com>
2016-11-03 16:55:23 +0800

    会话的cell的高度

--- kefu/CustomerMessageListViewController.m
+++ kefu/CustomerMessageListViewController.m
@@ -40,7 +40,7 @@
 #define RGBACOLOR(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f \
 alpha:(a)]
 
-#define kConversationCellHeight         60
+#define kConversationCellHeight         68
 
 @interface CustomerMessageListViewController()<UITableViewDelegate, UITableViewDataSource,
 TCPConnectionObserver, CustomerMessageObserver, SystemMessageObserver>
