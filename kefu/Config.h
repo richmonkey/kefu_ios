478ab0cc40f6d2dec0991013472679835fc08487
houxh <houxuehua49@gmail.com>
2018-04-03 13:08:08 +0800

    version 1.3.0

--- kefu/Config.h
+++ kefu/Config.h
@@ -9,11 +9,21 @@
 #ifndef Config_h
 #define Config_h
 
+#if 0
+//开发
+#define APPID 1453
+#define IM_API @"http://192.168.33.10:5000"
+#define IM_HOST @"192.168.33.10"
+#define KEFU_API @"http://192.168.33.10:60001"
+#else
 //外网
 #define APPID 1670
 #define IM_API @"http://api.gobelieve.io"
 #define IM_HOST @"imnode2.gobelieve.io"
 #define KEFU_API @"http://api.kefu.gobelieve.io"
+#endif
+
+
 #define REGISTER_URL @"https://xiaowei.io"
 //小微客服本身对应的咨询台
 #define XIAOWEI_STORE_ID 110
